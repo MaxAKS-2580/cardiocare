be4cb37ac57a94edd5fe8c1a72fd3b26f3f2cec5
MaxAKS-2580 <136919042+MaxAKS-2580@users.noreply.github.com>
2024-10-27 06:28:14 +0530

    Update settings.py

--- hf/hacktober/settings.py
+++ hf/hacktober/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Include your static files directory
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
